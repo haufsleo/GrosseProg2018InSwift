c217177ec3222d5c14856a16f5309d780c15db30
Leo Haufs <haufs@me.com>
2018-06-13 22:20:28 +0200

    Initial Commit

---
+++ Große Prog 2018/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Große Prog 2018
+//
+//  Created by Leo Haufs on 13.06.18.
+//  Copyright © 2018 Leo Haufs. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
